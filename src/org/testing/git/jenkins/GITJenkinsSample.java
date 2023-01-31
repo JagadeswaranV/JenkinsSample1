a9b840f82918e523fbace0d218ffd29ebba4b30a
JagadeswaranV <jagadesv27@gmail.com>
2023-01-31 21:26:14 +0530

    First Push

---
+++ src/org/testing/git/jenkins/GITJenkinsSample.java
@@ -0,0 +1,12 @@
+package org.testing.git.jenkins;
+
+public class GITJenkinsSample {
+	public static void main(String[] args) {
+		System.out.println("1) Om Nama Shivaya");
+		System.out.println("2) Om Nama Shivaya");
+		System.out.println("3) Om Nama Shivaya");
+		System.out.println("4) Om Nama Shivaya");
+		System.out.println("5) Om Nama Shivaya");
+	}
+
+}
